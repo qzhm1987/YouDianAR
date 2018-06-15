e77b8b6f3beb4cb67767959bd5e1f53ba9483331
youdian <youdian@youdian.local>
2018-06-15 10:17:38 +0800

    2带我去

---
+++ Category/NSString+YD_AR.m
@@ -0,0 +1,108 @@
+//
+//  NSString+YD_AR.m
+//  YouDian_AR
+//
+//  Created by youdian on 2018/6/15.
+//  Copyright © 2018年 YouDian. All rights reserved.
+//
+
+#import "NSString+YD_AR.h"
+
+@implementation NSString (YD_AR)
+
+
+- (NSString *)currentTimeStr{
+    NSDate* date = [NSDate dateWithTimeIntervalSinceNow:0];//获取当前时间0秒后的时间
+    NSTimeInterval time=[date timeIntervalSince1970]*1000;// *1000 是精确到毫秒，不乘就是精确到秒
+    NSString *timeString = [NSString stringWithFormat:@"%.0f", time];
+    return timeString;
+}
+
+//加密
+-(NSData *)AES256ParmEncryptWithKey:(NSString *)key Encrypttext:(NSData *)text{
+    char keyPtr[kCCKeySizeAES256+1];
+    bzero(keyPtr, sizeof(keyPtr));
+    [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
+    NSUInteger dataLength = [text length];
+    size_t bufferSize = dataLength + kCCBlockSizeAES128;
+    void *buffer = malloc(bufferSize);
+    size_t numBytesEncrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCEncrypt, kCCAlgorithmAES128,
+                                          kCCOptionPKCS7Padding | kCCOptionECBMode,
+                                          keyPtr, kCCBlockSizeAES128,
+                                          NULL,
+                                          [text bytes], dataLength,
+                                          buffer, bufferSize,
+                                          &numBytesEncrypted);
+    if (cryptStatus == kCCSuccess) {
+        return [NSData dataWithBytesNoCopy:buffer length:numBytesEncrypted];
+    }
+    free(buffer);
+    return nil;
+}
+//解密
+- (NSData *)AES256ParmDecryptWithKey:(NSString *)key Decrypttext:(NSData *)text {
+    char keyPtr[kCCKeySizeAES256+1];
+    bzero(keyPtr, sizeof(keyPtr));
+    [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
+    NSUInteger dataLength = [text length];
+    size_t bufferSize = dataLength + kCCBlockSizeAES128;
+    void *buffer = malloc(bufferSize);
+    size_t numBytesDecrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt, kCCAlgorithmAES128,
+                                          kCCOptionPKCS7Padding | kCCOptionECBMode,
+                                          keyPtr, kCCBlockSizeAES128,
+                                          NULL,
+                                          [text bytes], dataLength,
+                                          buffer, bufferSize,
+                                          &numBytesDecrypted);
+    if (cryptStatus == kCCSuccess) {
+        return [NSData dataWithBytesNoCopy:buffer length:numBytesDecrypted];
+    }
+    free(buffer);
+    return nil;
+}
+
+-(NSString *) aes256_encrypt:(NSString *)key Encrypttext:(NSString *)text{
+    const char *cstr = [text cStringUsingEncoding:NSUTF8StringEncoding];
+    NSData *data = [NSData dataWithBytes:cstr length:text.length];
+    //对数据进行加密
+    NSData *result = [self AES256ParmEncryptWithKey:key Encrypttext:data];
+    
+    //转换为2进制字符串
+    if (result && result.length > 0) {
+        
+        Byte *datas = (Byte*)[result bytes];
+        NSMutableString *output = [NSMutableString stringWithCapacity:result.length * 2];
+        for(int i = 0; i < result.length; i++){
+            [output appendFormat:@"%02x", datas[i]];
+        }
+        return output;
+    }
+    return nil;
+}
+
+-(NSString *) aes256_decrypt:(NSString *)key Decrypttext:(NSString *)text{
+    //转换为2进制Data
+    NSMutableData *data = [NSMutableData dataWithCapacity:text.length / 2];
+    unsigned char whole_byte;
+    char byte_chars[3] = {'\0','\0','\0'};
+    int i;
+    for (i=0; i < [text length] / 2; i++) {
+        byte_chars[0] = [text characterAtIndex:i*2];
+        byte_chars[1] = [text characterAtIndex:i*2+1];
+        whole_byte = strtol(byte_chars, NULL, 16);
+        [data appendBytes:&whole_byte length:1];
+    }
+    
+    //对数据进行解密
+    NSData* result = [self  AES256ParmDecryptWithKey:key Decrypttext:data];
+    if (result && result.length > 0) {
+        return [[NSString alloc] initWithData:result encoding:NSUTF8StringEncoding];
+    }
+    return nil;
+}
+
+
+
+@end

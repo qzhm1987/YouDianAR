b4a4e056c992842f21e0383efa31ef09f6d0948a
youdian <youdian@YouDian.local>
2018-06-19 09:55:43 +0800

    Removed cached files

---
+++ View/QView.h
@@ -0,0 +1,13 @@
+//
+//  QView.h
+//  YouDian_AR
+//
+//  Created by youdian on 2018/6/15.
+//  Copyright © 2018年 YouDian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QView : UIView
+
+@end
---
+++ View/QView.m
@@ -0,0 +1,21 @@
+//
+//  QView.m
+//  YouDian_AR
+//
+//  Created by youdian on 2018/6/15.
+//  Copyright © 2018年 YouDian. All rights reserved.
+//
+
+#import "QView.h"
+
+@implementation QView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
